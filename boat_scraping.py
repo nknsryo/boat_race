c48f15b78aff7c1ed64475f8e04386aa5ce67425
nknsryo <nkns.ryo@gmail.com>
2022-02-03 22:40:03 +0900

    関数化しました。

--- boat_scraping.py
+++ boat_scraping.py
@@ -13,29 +13,21 @@ from selenium.webdriver.common.by import By
 # noinspection PyUnresolvedReferences
 from selenium.webdriver.common.keys import Keys
 
-
-def chromedriver_options():
-    # オプション設定
-    options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')  # ヘッドレスモード
-    # return options
+driver = webdriver.Chrome()
 
 
-def main():
-    # chromedriver_options()
-
-    # driver = webdriver.Chrome(options=chromedriver_options())
-    driver = webdriver.Chrome()
-
+# webページを開くところまで。(driverは外に定義してある)
+def web_open():
     driver.get(f"https://kyoteibiyori.com/race_shusso.php?place_no=1&race_no=1&hiduke=20220203&slider=1")
     driver.implicitly_wait(10)
-
 
     driver.find_element(By.ID, "wakubetsu2").click()
 
     driver.implicitly_wait(5)
 
-    # 1着率のリスト
+
+# 1着率のリストを作る
+def first():
     ittyaku = []
 
     ittyaku_ritsu = driver.find_element(By.XPATH,
@@ -53,29 +45,33 @@ def main():
 
     print(ittyaku)
 
-
-k
     time.sleep(1)
 
-# 2着率のリスト
-    nittyaku = []
 
-    nittyaku_ritsu = driver.find_element(By.XPATH,
-                                         "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[6]/td").text
+# 2着率のリストを作る
+def second():
+    ittyaku = []
+
+    ittyaku_ritsu = driver.find_element(By.XPATH,
+                                        "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[1]/td").text
 
     time.sleep(3)
 
-    nittyaku.append(nittyaku_ritsu)
+    ittyaku.append(ittyaku_ritsu)
 
     time.sleep(1)
-    for count_up_2 in range(6):
-        second = driver.find_element(By.XPATH,
-                                     f"/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[9]/td[{count_up_2 + 2}]").text
-        nittyaku.append(second)
+    for count_up in range(6):
+        first = driver.find_element(By.XPATH,
+                                    f"/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[{count_up + 2}]").text
+        ittyaku.append(first)
 
-    print(nittyaku)
+    print(ittyaku)
+
+    time.sleep(1)
+
 
 # 決まり手
+def third_kimarite():
     kimarite = []
 
     nige_ritsu = driver.find_element(By.XPATH,
@@ -96,6 +92,15 @@ k
 
     print(kimarite)
 
+
+def main():
+    web_open()
+    # 1着率のリスト
+    first()
+    # 2着率のリスト
+    second()
+    # 決まり手
+    third_kimarite()
     driver.close()
 
 
