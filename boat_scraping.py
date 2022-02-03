cb704b33eceb74026cf79f711a6577a99395ea93
nknsryo <nkns.ryo@gmail.com>
2022-02-04 00:44:22 +0900

    テスト版完成

--- boat_scraping.py
+++ boat_scraping.py
@@ -16,91 +16,72 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome()
 
 
-# webページを開くところまで。(driverは外に定義してある)
-def web_open():
-    driver.get(f"https://kyoteibiyori.com/race_shusso.php?place_no=1&race_no=1&hiduke=20220203&slider=1")
+def main():
+    driver.get("https://kyoteibiyori.com/")
+    # 1番目の開催場(桐生)のアイコン
+    hold_place = driver.find_element(By.XPATH, f"/html/body/div[3]/div/section[1]/div[2]/ul/li[1]/a/div[1]")
+    # 1番目の開催場の名前をテキスト化する
+    hold_place_name = driver.find_element(By.XPATH, f"/html/body/div[3]/div/section[1]/div[2]/ul/li[1]/a/div[1]").text
+    hold_place.click()
+    # 1R目の枠別勝率をクリック
+    driver.find_element(By.XPATH, f"/html/body/div[5]/div[1]/section/div[3]/div[2]/table/tbody/tr[1]/td[2]/a").click()
     driver.implicitly_wait(10)
-
     driver.find_element(By.ID, "wakubetsu2").click()
 
     driver.implicitly_wait(5)
 
+    race_list = []
+    for number in range(12):
+        race_number = number + 1
+        race_list.append(hold_place)
+        race_list.append(race_number)
 
-# 1着率のリストを作る
-def first():
-    ittyaku = []
+        # 1着率のリスト
 
-    ittyaku_ritsu = driver.find_element(By.XPATH,
-                                        "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[1]/td").text
+        first_place = driver.find_element(By.XPATH,
+                                          "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[1]/td").text
+        race_list.append(first_place)
 
-    time.sleep(3)
+        for count_up in range(6):
+            first_place_player_each = driver.find_element(By.XPATH,
+                                                          f"/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[{count_up + 2}]").text
+            race_list.append(first_place_player_each)
 
-    ittyaku.append(ittyaku_ritsu)
+            time.sleep(1)
 
-    time.sleep(1)
-    for count_up in range(6):
-        first = driver.find_element(By.XPATH,
-                                    f"/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[{count_up + 2}]").text
-        ittyaku.append(first)
+            # 2着率のリスト
 
-    print(ittyaku)
+            second_place = driver.find_element(By.XPATH,
+                                               "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[6]/td").text
+            race_list.append(second_place)
 
-    time.sleep(1)
+            time.sleep(1)
 
+            for count_up in range(6):
+                second_place_player_each = driver.find_element(By.XPATH,
+                                                               f"/html/body/div[8]/div[1]/section/"
+                                                               f"div[5]/table[1]/tbody/tr[9]/td"
+                                                               f"[{count_up + 2}]").text
+                race_list.append(second_place_player_each)
 
-# 2着率のリストを作る
-def second():
-    nittyaku = []
+                time.sleep(1)
 
-    ittyaku_ritsu = driver.find_element(By.XPATH,
-                                        "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[6]/td").text
+                run_away_probability = driver.find_element(By.XPATH,
+                                                           "/html/body/div[8]/div[1]/section/"
+                                                           "div[5]/table[1]/tbody/tr[26]/"
+                                                           "td").text
+                race_list.append(run_away_probability)
+                time.sleep(1)
+                kimarite_1 = driver.find_element(By.XPATH,
+                                                 f"/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[29]/td[1]").text
+                race_list.append(kimarite_1)
 
-    time.sleep(3)
+                kimarite_2 = driver.find_element(By.XPATH,
+                                                 f"/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[40]/td[2]").text
+                race_list.append(kimarite_2)
 
-    nittyaku.append(ittyaku_ritsu)
+        print(race_list)
 
-    time.sleep(1)
-    for count_up in range(6):
-        first = driver.find_element(By.XPATH,
-                                    f"/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[9]/td[{count_up + 2}]").text
-        nittyaku.append(first)
-
-    print(nittyaku)
-
-    time.sleep(1)
-
-
-# 決まり手
-def third_kimarite():
-    kimarite = []
-
-    nige_ritsu = driver.find_element(By.XPATH,
-                                     "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[26]/td").text
-
-    time.sleep(3)
-
-    kimarite.append(nige_ritsu)
-
-    time.sleep(1)
-    kimarite_1 = driver.find_element(By.XPATH,
-                                     f"/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[29]/td[1]").text
-    kimarite.append(kimarite_1)
-
-    kimarite_2 = driver.find_element(By.XPATH,
-                                     f"/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[40]/td[2]").text
-    kimarite.append(kimarite_2)
-
-    print(kimarite)
-
-
-def main():
-    web_open()
-    # 1着率のリスト
-    first()
-    # 2着率のリスト
-    second()
-    # 決まり手
-    third_kimarite()
     driver.close()
 
 
