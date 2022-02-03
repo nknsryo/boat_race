5aec5c5c55aa6e84e125358a9a7fb5cffcc4dfff
nknsryo <nkns.ryo@gmail.com>
2022-02-03 18:29:28 +0900

    ヘッドレスモードだとうまくいかない件。
    どうやったらfor文でxpathの中にある数値を回せるのかがわからない。
    find_elementsにして "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]"までにしてもできない。

--- boat_scraping.py
+++ boat_scraping.py
@@ -17,31 +17,61 @@ from selenium.webdriver.common.keys import Keys
 def chromedriver_options():
     # オプション設定
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # ヘッドレスモード
+    # options.add_argument('--headless')  # ヘッドレスモード
+    # return options
 
 
 def main():
     chromedriver_options()
+
     data_place = []
+    # data_placeはlistだよ！
+
+    # driver = webdriver.Chrome(options=chromedriver_options())
     driver = webdriver.Chrome()
+
     driver.get("https://kyoteibiyori.com/race_shusso.php?place_no=1&race_no=1&hiduke=20220203&slider=1")
+
     driver.implicitly_wait(10)
+
     driver.find_element(By.ID, "wakubetsu2").click()
-    time.sleep(2)
-    one = driver.find_element(By.XPATH, "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[1]/td")
+
+    driver.implicitly_wait(5)
+
+    one = driver.find_element(By.XPATH, "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[1]/td").text
+
+    time.sleep(3)
+
     data_place.append(one)
-    # A = driver.find_element(By.XPATH, "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[2]/text()")
-    # data_place.append(A)
-    # B = driver.find_element(By.XPATH, "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[3]/text()")
-    # data_place.append(B)
-    # C = driver.find_element(By.XPATH, "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[4]/text()")
-    # data_place.append(C)
-    # D = driver.find_element(By.XPATH, "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[5]/text()")
-    # data_place.append(D)
-    # E = driver.find_element(By.XPATH, "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[6]/text()")
-    # data_place.append(E)
-    # F = driver.find_element(By.XPATH, "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[7]/text()")
-    # data_place.append(F)
+
+    time.sleep(1)
+
+    a = driver.find_element(By.XPATH,
+                            "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[2]").text
+    data_place.append(a)
+
+    b = driver.find_element(By.XPATH,
+                            "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[3]").text
+    data_place.append(b)
+
+    c = driver.find_element(By.XPATH,
+                            "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[4]").text
+    data_place.append(c)
+
+    d = driver.find_element(By.XPATH,
+                            "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[5]").text
+    data_place.append(d)
+
+    e = driver.find_element(By.XPATH,
+                            "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[6]").text
+    data_place.append(e)
+
+    f = driver.find_element(By.XPATH,
+                            "/html/body/div[8]/div[1]/section/div[5]/table[1]/tbody/tr[4]/td[7]").text
+    data_place.append(f)
+    # 48~70 forでまとめる
+
+    time.sleep(1)
 
     print(data_place)
 
